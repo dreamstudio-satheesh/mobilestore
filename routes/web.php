27b031fd2ebe1292ea8c1f950c5292e7cdc013f7
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-01-20 16:50:15 +0530

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',  [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm']);
+
+Auth::routes([
+
+    'register' => false, // Register Routes...
+  
+    'reset' => false, // Reset Password Routes...
+  
+    'verify' => false, // Email Verification Routes...
+  
+  ]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
